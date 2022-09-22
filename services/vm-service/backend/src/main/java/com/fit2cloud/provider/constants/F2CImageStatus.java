cb219b67665ef463d6be5c7c1d688ba8d9edf6d9
zhangshaohu <5563247+zhangshaohu@user.noreply.gitee.com>
2022-09-22 14:21:18 +0800

    fix(vm-service): 添加镜像状态,修改同步入库方法

---
+++ services/vm-service/backend/src/main/java/com/fit2cloud/provider/constants/F2CImageStatus.java
@@ -0,0 +1,18 @@
+package com.fit2cloud.provider.constants;
+
+/**
+ * @Author:张少虎
+ * @Date: 2022/9/22  10:42 AM
+ * @Version 1.0
+ * @注释:
+ */
+public class F2CImageStatus {
+    /**
+     * 正常状态
+     */
+    public static final String normal = "NORMAL";
+    /**
+     * 删除状态
+     */
+    public static final String deleted = "DELETED";
+}
