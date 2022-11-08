f6cbb69c9ddc9f5d18fd1bbae841ba7691c4cb42
ulleo <ulleo.wang@gmail.com>
2022-11-08 11:21:57 +0800

    fix: 网关启动失败

--- framework/gateway/src/main/java/com/fit2cloud/gateway/config/ServiceConfig.java
+++ framework/gateway/src/main/java/com/fit2cloud/gateway/config/ServiceConfig.java
@@ -1,9 +1,16 @@
 package com.fit2cloud.gateway.config;
 
+import com.fit2cloud.service.PerfMonitorService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
-@ComponentScan(basePackages = {"com.fit2cloud.service", "com.fit2cloud.base.service", "com.fit2cloud.common.scheduler"})
+@ComponentScan(
+        basePackages = {"com.fit2cloud.service", "com.fit2cloud.base.service", "com.fit2cloud.common.scheduler"},
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {PerfMonitorService.class})
+        }
+)
 @Configuration
 public class ServiceConfig {
 
