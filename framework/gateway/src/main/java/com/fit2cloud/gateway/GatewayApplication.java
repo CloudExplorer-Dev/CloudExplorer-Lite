02c246c5d4986a94304249c1a3fbc817a464cb72
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2022-11-22 16:53:11 +0800

    fix(sdk): 添加全局事件总线,网关排除RedisConfig配置,添加基座模块选中状态,模块菜单默认路由到有权限的第一个菜单

--- framework/gateway/src/main/java/com/fit2cloud/gateway/GatewayApplication.java
+++ framework/gateway/src/main/java/com/fit2cloud/gateway/GatewayApplication.java
@@ -1,31 +1,1 @@
-package com.fit2cloud.gateway;
-
-import com.fit2cloud.autoconfigure.JobSettingConfig;
-import com.fit2cloud.autoconfigure.LocaleConfig;
-import com.fit2cloud.autoconfigure.SecurityConfig;
-import com.fit2cloud.autoconfigure.SwaggerConfig;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.PropertySource;
-
-@SpringBootApplication(exclude = {
-        SecurityConfig.class,
-        ServletWebServerFactoryAutoConfiguration.class,
-        JobSettingConfig.class,
-        LocaleConfig.class,
-        SwaggerConfig.class
-})
-@EnableDiscoveryClient
-@PropertySource(value = {
-        "classpath:commons.properties",
-        "file:${ce.config.file}"
-}, encoding = "UTF-8", ignoreResourceNotFound = true)
-public class GatewayApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
-    }
-
-}
+package com.fit2cloud.gateway;import com.fit2cloud.autoconfigure.*;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;import org.springframework.cloud.client.discovery.EnableDiscoveryClient;import org.springframework.context.annotation.PropertySource;@SpringBootApplication(exclude = {        SecurityConfig.class,        ServletWebServerFactoryAutoConfiguration.class,        JobSettingConfig.class,        LocaleConfig.class,        SwaggerConfig.class,        RedisConfig.class})@EnableDiscoveryClient@PropertySource(value = {        "classpath:commons.properties",        "file:${ce.config.file}"}, encoding = "UTF-8", ignoreResourceNotFound = true)public class GatewayApplication {    public static void main(String[] args) {        SpringApplication.run(GatewayApplication.class, args);    }}
