c70b57bf22fdefc51f5e8bf322c5937437086981
liudi <liudi96@126.com>
2022-12-30 18:17:46 +0800

    fix: 添加磁盘页面，磁盘大小没有单位

--- services/vm-service/backend/src/main/java/com/fit2cloud/provider/impl/vsphere/entity/request/VsphereCreateDiskForm.java
+++ services/vm-service/backend/src/main/java/com/fit2cloud/provider/impl/vsphere/entity/request/VsphereCreateDiskForm.java
@@ -15,7 +15,7 @@ public class VsphereCreateDiskForm {
     private String diskType;
     @Form(inputType = InputType.Radio, label = "磁盘模式", defaultValue = "independent_persistent", textField = "name", valueField = "id", method = "getDiskModes", clazz = VsphereCloudProvider.class)
     private String diskMode;
-    @Form(inputType = InputType.Number, label = "磁盘大小", defaultValue = "10", attrs = "{\"min\":10,\"max\":32728,\"step\":10}")
+    @Form(inputType = InputType.Number, label = "磁盘大小", defaultValue = "10", attrs = "{\"min\":10,\"max\":32728,\"step\":10}",unit = "GB")
     private Long size;
     @Form(inputType = InputType.Radio, label = "存储器", defaultValue = "only-a-flag", textField = "name", valueField = "id", method = "getDatastoreTypes", clazz = VsphereCloudProvider.class)
     private String datastoreType;
