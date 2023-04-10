5fcf449885179a6d943319e3caec26a9aa9f7ee2
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2023-04-10 10:37:35 +0800

    fix(vm-service): 华为云包年包月云盘扩容，无法自动完成支付问题

--- services/vm-service/backend/src/main/java/com/fit2cloud/provider/impl/huawei/entity/request/HuaweiResizeDiskRequest.java
+++ services/vm-service/backend/src/main/java/com/fit2cloud/provider/impl/huawei/entity/request/HuaweiResizeDiskRequest.java
@@ -1,7 +1,7 @@
 package com.fit2cloud.provider.impl.huawei.entity.request;
 
 import com.fit2cloud.provider.entity.request.BaseDiskResizeRequest;
-import com.huaweicloud.sdk.ecs.v2.model.DetachServerVolumeRequest;
+import com.huaweicloud.sdk.evs.v2.model.BssParamForResizeVolume;
 import com.huaweicloud.sdk.evs.v2.model.OsExtend;
 import com.huaweicloud.sdk.evs.v2.model.ResizeVolumeRequest;
 import com.huaweicloud.sdk.evs.v2.model.ResizeVolumeRequestBody;
@@ -20,6 +20,10 @@ public class HuaweiResizeDiskRequest extends BaseDiskResizeRequest {
         ResizeVolumeRequestBody body = new ResizeVolumeRequestBody();
         OsExtend osExtend = new OsExtend();
         osExtend.setNewSize((int) super.getNewDiskSize());
+        //自动扣费参数，只对包周期有意义，对按需无影响
+        BssParamForResizeVolume bssParamBody = new BssParamForResizeVolume();
+        bssParamBody.withIsAutoPay(BssParamForResizeVolume.IsAutoPayEnum.TRUE);
+        body.withBssParam(bssParamBody);
         body.setOsExtend(osExtend);
         resizeVolumeRequest.withBody(body);
         return resizeVolumeRequest;
