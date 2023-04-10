829aaf4bb1c2e3d64583c3fe1bbb925e6956bf2b
ulleo <ulleo.wang@gmail.com>
2023-04-10 11:21:26 +0800

    fix: 加密错误导致云账号信息不能解密

--- framework/management-center/backend/src/main/java/com/fit2cloud/dao/entity/CloudAccount.java
+++ framework/management-center/backend/src/main/java/com/fit2cloud/dao/entity/CloudAccount.java
@@ -4,23 +4,19 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import com.baomidou.mybatisplus.core.handlers.MybatisEnumTypeHandler;
-import com.fit2cloud.dao.handler.CredentialTypeHandler;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
-import java.io.Serial;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.fit2cloud.common.constants.CloudAccountConstants;
+import com.fit2cloud.base.handler.CredentialTypeHandler;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -33,7 +29,7 @@ import com.fit2cloud.common.constants.CloudAccountConstants;
 @Getter
 @Setter
 @Accessors(chain = true)
-@TableName(value = "cloud_account",resultMap = "BaseResultMap")
+@TableName(value = "cloud_account", resultMap = "BaseResultMap")
 public class CloudAccount implements Serializable {
 
     @Serial
