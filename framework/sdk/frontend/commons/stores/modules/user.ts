3ea2b07e9d58452f40c41333a6cb2a4971ec76cc
ulleo <ulleo.wang@gmail.com>
2023-04-19 18:12:07 +0800

    fix: logout error

--- framework/sdk/frontend/commons/stores/modules/user.ts
+++ framework/sdk/frontend/commons/stores/modules/user.ts
@@ -126,7 +126,7 @@ export const useUserStore = defineStore({
     },
     doLogout(redirect?: string) {
       //logout
-      logout(this.userStoreObject?.user.username).finally(() => {
+      logout(this.userStoreObject?.user?.username).finally(() => {
         this.clear();
 
         if (
